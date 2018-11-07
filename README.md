5451f5e11a4f9303d7e7e68c41f9b80457dce321
Justin Wong <11310624+wongjn@users.noreply.github.com>
2018-11-07 15:03:17 +0000

    Initial Drupal 8 implementation

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Google Maps Embed
 
-Provides a formatter to display field values using the
-[Google Map Embed API](https://developers.google.com/maps/documentation/embed).
-This API has unlimited free usage limit but has fewer capabilities than the
-[JavaScript API](https://developers.google.com/maps/documentation/javascript/).
+Provides a formatter to display field values using the [Google Map Embed API](https://developers.google.com/maps/documentation/embed).
+This API has unlimited free usage limit but has fewer capabilities than the [JavaScript API](https://developers.google.com/maps/documentation/javascript/).
 
 ## Supported Field Types
 
-- [`addressfield`](https://drupal.org/project/google_map_field)
-- [`google_map_field`](https://drupal.org/project/google_map_field)
+- [`address`](https://drupal.org/project/address)
 
 ## Initial Setup
 
-One must set the API key variable `google_map_embed_api_key` with the value of
-an API key that has the Embed API enabled.
+One must set the site setting `google_map_embed_api_key` with the value of
+an API key that has the Embed API enabled:
+
+```php
+$settings['google_map_embed_api_key'] = 'api_key_here';
+```
---
+++ src/Plugin/Field/FieldFormatter/GoogleMapEmbedAddressFormatter.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Drupal\google_map_embed\Plugin\Field\FieldFormatter;
+
+use CommerceGuys\Addressing\AddressFormat\AddressFormatRepositoryInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\geolocation\GoogleMapsDisplayTrait;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\geolocation\GeolocationItemTokenTrait;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\address\Plugin\Field\FieldFormatter\AddressDefaultFormatter;
+
+/**
+ * Plugin implementation of the 'google_map_embed_address' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "google_map_embed_address",
+ *   label = @Translation("Google Map (Embed API)"),
+ *   field_types = {
+ *     "address"
+ *   }
+ * )
+ */
+class GoogleMapEmbedAddressFormatter extends AddressDefaultFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'width' => '',
+      'height' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+
+    $elements['width'] = [
+      '#title' => $this->t('Width'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('width'),
+    ];
+
+    $elements['height'] = [
+      '#title' => $this->t('Height'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('height'),
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $settings = $this->getSettings();
+    $summary = parent::settingsSummary();
+
+    if ($settings['width']) {
+      $summary[] = $this->t('Width: @width', ['@width' => $settings['width']]);
+    }
+
+    if ($settings['height']) {
+      $summary[] = $this->t('Height: @height', ['@height' => $settings['height']]);
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $settings = $this->getSettings();
+
+    foreach ($items as $delta => $item) {
+      $country_code = $item->getCountryCode();
+      $address_format = $this->addressFormatRepository->get($country_code);
+      $values = $this->getValues($item, $address_format);
+
+      $address_components = [];
+      foreach ($address_format->getUsedFields() as $field) {
+        if ($values[$field]) {
+          $address_components[] = urlencode($values[$field]);
+        }
+      }
+
+      $map = [
+        '#theme' => 'google_map_embed',
+        '#place' => implode(',', $address_components),
+      ];
+
+      if ($settings['width']) {
+        $map['#attributes']['width'] = $settings['width'];
+      }
+      if ($settings['height']) {
+        $map['#attributes']['height'] = $settings['height'];
+      }
+
+      $elements[$delta] = $map;
+    }
+
+    return $elements;
+  }
+
+}
